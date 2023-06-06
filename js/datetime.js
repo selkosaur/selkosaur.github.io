d6e7594444a75d60b105ab607089a36704cd6f1f
so <jwthecowicorn@gmail.com>
2023-06-06 03:58:03 -0400

    date display added

--- js/datetime.js
+++ js/datetime.js
@@ -136,3 +136,41 @@ export function dayvsnight(datetime, sunrisedatetime, sunsetdatetime) {
   dvn.afterSet = datetime > sunsetdatetime ? true : false;
   return dvn;
 }
+/**
+ * display current date
+ * @param {HTMLElement|String} targetEl the element to display the date
+ * @returns
+ */
+const dateDisplay = (targetEl) => {
+  if (typeof targetEl == "string") {
+    targetEl = document.querySelector(targetEl);
+  }
+  if (!targetEl) {
+    console.error("no target element found");
+    return;
+  }
+  let wrapper = document.createElement("span");
+  wrapper.classList.add("current-date");
+  targetEl.appendChild(wrapper);
+
+  const formatDate = () => {
+    let d = new Date();
+    let str = d.toLocaleDateString([], {
+      weekday: "long",
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    });
+    str = str.replace(/,/g, `</span><span>,`);
+    str = `<span>${str}</span>`;
+    return str;
+  };
+
+  const insertHTML = () => {
+    wrapper.innerHTML = formatDate();
+  };
+  insertHTML();
+  const update = setInterval(insertHTML, 1000);
+};
+
+export { dateDisplay };
