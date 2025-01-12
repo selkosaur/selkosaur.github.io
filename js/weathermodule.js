34663d2e9cebf91ff045a26ea71074640792af57
so <jwthecowicorn@gmail.com>
2025-01-11 19:38:50 -0500

    removing console log

--- js/weathermodule.js
+++ js/weathermodule.js
@@ -73,7 +73,6 @@ export class WeatherModule {
     // Storing data in form of JSON
     const data = await response.json();
     if (response) {
-      console.log("response is true");
       callbackfn(data);
     }
   }
@@ -119,9 +118,7 @@ export class WeatherModule {
     );
   }
   weatherLocation() {
-    let mode = this.locationmode;
-    console.log("mode: ");
-    console.log(mode);
+    const mode = this.locationmode;
     if (mode.name === "auto" || undefined) {
       this.geolocatorPrompt();
     }
@@ -138,7 +135,7 @@ export class WeatherModule {
   }
   weatherfetch() {
     let weathurl = `https://api.open-meteo.com/v1/forecast?latitude=${this.lat}&longitude=${this.long}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weathercode,cloudcover,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`;
-    console.log("weatherurl: %s", weathurl);
+
     this.getapi(weathurl, this.callweatherbuilder.bind(this));
   }
   savelatlong(arraydata) {
